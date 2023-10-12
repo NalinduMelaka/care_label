3096ae5831cc66febf58e5cbb8236e52b0b37e0f
NalinduMelaka <nalindumelaka@outlook.com>
2023-10-12 11:10:35 +0530

    td_12_23_2

--- src/app/components/Care.tsx
+++ src/app/components/Care.tsx
@@ -6,16 +6,16 @@ type Props = {}
 
 const Care = (props: Props) => {
   return (
-    <table className="border-separate border border-slate-500 text-xs">
+    <table className="border-collapse border border-slate-500 text-xs">
       <thead>
         <tr>
-          <th className='border border-black font-thin w-14'>Ref No</th>
-          <th className='border border-black font-thin w-14'>Wash Symbol</th>
-          <th className='border border-black font-thin w-28'>Fibre</th>
-          <th className='border border-black font-thin w-14'>Zoodes</th>
-          <th className='border border-black font-thin w-14'>COO</th>
-          <th className='border border-black font-thin w-14'>Caretext</th>
-          <th className='border border-black font-thin w-14'>MPART/FW</th>
+          <th className='border border-black w-14'>Ref No</th>
+          <th className='border border-black w-14'>Wash Symbol</th>
+          <th className='border border-black w-28'>Fibre</th>
+          <th className='border border-black w-14'>Zoodes</th>
+          <th className='border border-black w-14'>COO</th>
+          <th className='border border-black w-14'>Caretext</th>
+          <th className='border border-black w-14'>MPART/FW</th>
         </tr>
       </thead>
       <tbody>
